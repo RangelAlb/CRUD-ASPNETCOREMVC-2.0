5f6b31664ded3ea35ac28e73746db39aa785bd50
RangelAlb <rangel_albuquerque_2012@hotmail.com>
2021-01-05 07:47:33 -0300

    Crud em AspNetCore

---
+++ Crud AspNet Core/WebApplication1/WebApplication1/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Display(Description = "Código")]
+        public int Id { get; set; }
+
+        [Display(Description = "Nome do Usuário")]
+        public string Nome { get; set; }
+
+        [Display(Description = "Idade")]
+        public int Idade { get; set; }
+
+        [Display(Description = "Tipo de Usuário")]
+        public int Tipo { get; set; }
+    }
+}
